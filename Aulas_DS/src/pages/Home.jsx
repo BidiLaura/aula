a3bb4595a4f47b0e5275d6cc517defddda6c949b
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-09-20 16:49:48 -0300

    auauau

---
+++ Aulas_DS/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import NavBar from "../componentes/NavBar";
+import PageContent from "../componentes/PageContent";
+import Footer from "../componentes/footer";
+import '../componentes/style.css'
+
+export default function Home() {
+    return (
+        <>
+        <NavBar/>
+        <PageContent/>
+        <Footer/>
+        </>
+    )
+}
